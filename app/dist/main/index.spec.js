9898c5b052fade3f0231f782e5b200a68c163839
DouglasVolcato <douglasvolcato@gmail.com>
2025-11-09 16:06:10 -0300

    refactor: add path aliases

---
+++ app/dist/main/index.spec.js
@@ -0,0 +1,6 @@
+"use strict";
+describe("Test running", () => {
+    test("Should pass", () => {
+        expect(1).toBe(1);
+    });
+});
--- app/jest.config.js
+++ app/jest.config.js
@@ -13,5 +13,14 @@ export default {
         "!**/*index.(t|j)s",
         "!**/*test.(t|j)s",
         "!**/dist/**",
-    ]
+    ],
+    moduleNameMapper: {
+        "^@main/(.*)$": "<rootDir>/src/main/$1",
+        "^@tests/(.*)$": "<rootDir>/src/tests/$1",
+        "^@db/(.*)$": "<rootDir>/src/db/$1",
+        "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+        "^@controllers/(.*)$": "<rootDir>/src/controllers/$1",
+        "^@views/(.*)$": "<rootDir>/src/views/$1",
+        "^@usecases/(.*)$": "<rootDir>/src/usecases/$1",
+    }
 };
